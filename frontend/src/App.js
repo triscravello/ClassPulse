f1db060523c1613b118eefcd9334dc1303294f36
Tristan Cravello <tlcravello@gmail.com>
2026-01-15 16:59:00 -0500

    Remove navbar and sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
-import { useContext } from "react";
-import NavBar from './components/layout/NavBar';
-import SideBar from './components/layout/Sidebar';
-import Footer from './components/layout/Footer';
-import AppRoutes from './AppRoutes';
+// App.js
+import AppRoutes from "./AppRoutes";
 import './App.css';
-import { UserContext } from "./context/UserContext";
 
 function App() {
-  const { user } = useContext(UserContext);
-
-  const showLayout = !!user; // Only show sidebar/navbar/footer if logged in
-
   return (
-    <div className='app-container'>
-      {showLayout && <SideBar collapsed={true} />}
-
-      <div className='content'>
-        {showLayout && <NavBar />}
+    <div className="app-container">
+      {/* Don't render sidebar/nav/footer here */}
+      <div className="content">
         <main>
           <AppRoutes />
         </main>
-        {showLayout && <Footer />}
       </div>
     </div>
   );
