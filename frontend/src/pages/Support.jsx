3bf7fc43ecd542e2d08da9bb11f873e232b8f0d5
Tristan Cravello <tlcravello@gmail.com>
2026-01-22 15:00:47 -0500

    Initial commit of Support

---
+++ frontend/src/pages/Support.jsx
@@ -0,0 +1,15 @@
+// frontend/src/pages/Support.jsx
+import styles from './Support.module.css';
+
+const Support = () => {
+  return (
+    <div className={styles.pageContainer}>
+      <h1>Support</h1>
+      <p>
+        This is your support page. Include FAQs, contact info, or a support form here.
+      </p>
+    </div>
+  );
+};
+
+export default Support;
