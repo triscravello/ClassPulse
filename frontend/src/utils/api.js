909f1ab8c441d648b6732e8fb90b53a22ebdca8d
Tristan Cravello <tlcravello@gmail.com>
2026-01-23 14:11:05 -0500

    Configure API base URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,11 @@
 // src/utils/api.js
 import axios from 'axios';
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001/api/v1';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 // Create an axios instance
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001/api/v1',
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
